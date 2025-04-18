ee9394a7df145bba5210b9a7ae31ee5114ffc9af
Luu Hai Long <longlanhlong123123@gmail.com>
2025-04-18 14:52:05 +0700

    Thay doi lan 2

--- src/Bai2_3.java
+++ src/Bai2_3.java
@@ -6,7 +6,7 @@ public class Bai2_3 {
         int n = sc.nextInt();
         for(int i = 2 ; i < n ; i++){
             if(n % i != 0){
-
+                System.out.println("Long dep trai");
             }
         }
     }
